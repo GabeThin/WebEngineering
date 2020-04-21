335e468699c03cc5ca964763f8cedbbc637f61cc
Gabe Thin <gathin@packer.edu>
2020-04-21 14:15:44 -0400

    Update script.js

--- caesar cipher/script.js
+++ caesar cipher/script.js
@@ -1,14 +1,40 @@
 let button = document.getElementById("encodeButton");
-button.addEventListener("click", rev);
+button.addEventListener("click", encode);
 
 
-function rev() {
+// function rev() {
+//   let string = document.getElementById("message").value;
+//   let splitString = string.split("");
+//
+//   let reverseArray = splitString.reverse();
+//   let reverseString = reverseArray.join("");
+//
+//   document.getElementById("encoded").innerHTML = reverseString;
+//
+// }
+
+function encode() {
   let string = document.getElementById("message").value;
-  let splitString = string.split("");
+  let encodedString = "";
+  let key = parseInt(document.getElementById("key").value);
 
-  let reverseArray = splitString.reverse();
-  let reverseString = reverseArray.join("");
+  for (var i = 0; i < string.length; i++) {
+    let charCode = string.charCodeAt(i) + key;
 
-  document.getElementById("encoded").innerHTML = reverseString;
+    if (string[i] == string[i].toUpperCase()) {
+      if(string.charCodeAt(i) + key > 90) {
+        charCode -= 26;
+      }
+    }
 
+    if (string[i] == string[i].toLowerCase()){
+      if(string.charCodeAt(i) + key > 122) {
+        charCode -= 26;
+      }
+    }
+
+    encodedString += String.fromCharCode(charCode);
+  }
+
+  document.getElementById("encoded").innerHTML = encodedString;
 }
