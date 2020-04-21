c10739b7b2cf0d0ef67dfa6c2e6c0a9dc8db51fc
Gabe Thin <gathin@packer.edu>
2020-04-21 11:01:45 -0400

    reverse function

--- caesar cipher/script.js
+++ caesar cipher/script.js
@@ -1,1 +1,14 @@
+let button = document.getElementById("encodeButton");
+button.addEventListener("click", rev);
 
+
+function rev() {
+  let string = document.getElementById("message").value;
+  let splitString = string.split("");
+
+  let reverseArray = splitString.reverse();
+  let reverseString = reverseArray.join("");
+
+  document.getElementById("encoded").innerHTML = reverseString;
+
+}
