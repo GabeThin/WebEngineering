6159c297d651b3f03227cb830b7fe02c17b0faf8
Gabe Thin <gathin@packer.edu>
2020-05-13 05:52:52 -0400

    sandwich read

--- firebase/script.js
+++ firebase/script.js
@@ -21,12 +21,12 @@ document.getElementById("submit").addEventListener("click", function() {
   var cheese = document.getElementById("cheese").value;
   var condiments = document.getElementById("condiments").value;
   var other = document.getElementById("other").value;
-  makeSandwich(id, bread, meat, cheese, other);
+  makeSandwich(id, bread, meat, cheese, condiments, other);
 });
 
 //Writes information to the database
-function makeSandwich(id, bread, meat, cheese, other) {
-  database.ref('users/' + id).set({
+function makeSandwich(id, bread, meat, condiments, cheese, other) {
+  database.ref('users/').push({
     name: id,
     bread: bread,
     meat: meat,
@@ -34,6 +34,36 @@ function makeSandwich(id, bread, meat, cheese, other) {
     cheese: cheese,
     other: other
   });
-
-  console.log("jfkld;sa");
 }
+
+firebase.database().ref('/').on('value', function(snapshot) {
+  //Get the value from the database snapshot
+  var users = snapshot.val().users;
+  //Get the location from html
+  var ul = document.getElementById("output-list");
+  //Clear the html so we don't duplicate everything
+  ul.innerHTML = "";
+  //Loop through the value and add each one
+  for (var i in users) {
+    var name = document.createElement("p");
+    name.innerHTML = users[i].name + "'s sandwich:"
+
+    var list1 = document.createElement("li");
+    list1.innerHTML = users[i].bread;
+    var list2 = document.createElement("li");
+    list2.innerHTML = users[i].meat;
+    var list3 = document.createElement("li");
+    list3.innerHTML = users[i].condiments;
+    var list4 = document.createElement("li");
+    list4.innerHTML = users[i].cheese;
+    var list5 = document.createElement("li");
+    list5.innerHTML = users[i].other;
+    ul.append(name);
+    ul.append(list1);
+    ul.append(list2);
+    ul.append(list3);
+    ul.append(list4);
+    ul.append(list5);
+    console.log(list3);
+}
+});
